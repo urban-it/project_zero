ef71c0ab016705946fd3058ce6bfd62f36f9587a
Conrad <grosserconrad@gmail.com>
2017-05-03 09:00:25 +0200

    Gamer Update 1
    - Adjusted and repaired the gamer.lua
    - Function call works now, functions for all values are existent
    - Renamed soundtrack folder for better structure

--- lua_testbay/gamer.lua
+++ lua_testbay/gamer.lua
@@ -9,20 +9,59 @@ require "peasant"
 
 -- Start of the gamer.lua program
 gpower = 0 gawe = 0 gworship = 0 glove = 0 gfear = 0 ghatred = 0
+playerstats = {gawe, gworship, glove, gfear, ghatred}	-- Create array for some cases of storage of the variables/ Idea for storage
 
 -- Functions to calculate the global stats
+function calc_gawe(civilisation)	-- This function adds up the awe of the civilisation
+	for i, peasant in ipairs(civilisation) do
+		gawe = peasant.awe + gawe
+	end
+	return gawe
+end
 
-function calc_glove(...)	-- NO not glove! g love. That´s what the function calcs.
-	for peasant in arg do
-		glove = arg[peasant] + glove
+function calc_gworship(civilisation)	-- This function adds up the worship of the civilisation
+	for i, peasant in ipairs(civilisation) do
+		gworship = peasant.worship + gworship
+	end
+	return gworship
+end
+
+function calc_glove(civilisation)	-- NO not glove! g love. That´s what the function calcs.
+	for i, peasant in ipairs(civilisation) do
+		glove = peasant.love + glove
 	end
 	return glove
+end
+
+function calc_gfear(civilisation)	-- This function adds up the fear of the civilisation
+	for i, peasant in ipairs(civilisation) do
+		gfear = peasant.fear + gfear
+	end
+	return gfear
+end
+
+function calc_ghatred(civilisation)	-- This function adds up the fear of the civilisation
+	for i, peasant in ipairs(civilisation) do
+		ghatred = peasant.hatred + ghatred
+	end
+	return ghatred
+end
+
+function calc_all(civilisation)	-- This function adds up all the values for one civilisation
+	for i, peasant in ipairs(civilisation) do
+		gawe = peasant.awe + gawe
+		gworship = peasant.worship + gworship
+		glove = peasant.love + glove
+		gfear = peasant.fear + gfear
+		ghatred = peasant.hatred + ghatred
+	end
+	return	-- No value jet
 end
 
 -- Testing area:
 
 peasant1 = birth(peasant1)
-change_love(peasant1, 50)
+change_love(peasant1, 20)
 print(peasant1.name)
 
 peasant2 = birth(peasant2)
@@ -33,7 +72,7 @@ peasant3 = birth(peasant3)
 change_love(peasant3, 130)
 print(peasant3.name)
 
-civilisation = {peasant1.love, peasant2.love, peasant3.love}
+civilisation = {peasant1, peasant2, peasant3}
 calc_glove(civilisation)
 print(glove)
 
@@ -41,5 +80,5 @@ print(glove)
 	Notes:
 	
 	Known errors:
-	Problem with the function call (variable arguments)
+		No variable for calc_all function
 ]]--
