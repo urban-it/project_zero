b26cb4dbcc7846016b81d0f3b2b38e56bd2ff527
Conrad <grosserconrad@gmail.com>
2017-05-04 13:31:43 +0200

    Peasant Update 4
    - Generalized the checkvalues and the change_value function
    - Adjusted the names to pregame status (need to be changed later)

--- lua_testbay/peasant.lua
+++ lua_testbay/peasant.lua
@@ -12,12 +12,24 @@ math.randomseed(os.time()) -- Seed the random function with the time
 math.random(); math.random(); math.random() -- To avoid an error, where the "random" number equals 1 generate 3 numbers first
 
 -- VARIABLES
-names = {"Daniel", "Bruno", "Clemens", "Toelpel", "Nichtsnutz", "Idiot", "Dumbaf"}	-- All possible names are cached in this table
+names = {"Tjorben", "Niemand", "Idina-Hui", "Toelpel", "Nichtsnutz", "Idiot", "Dumbaf"}	-- All possible names are cached in this table
 
 -- FUNCTIONS
 function checkvalues(peasant_id)
+	if peasant_id.awe > 100 then peasant_id.awe = 100	end	-- Check if the awe is over 100% and if yes correct it
+	if peasant_id.awe < 0 then peasant_id.awe = 0	end	-- Check if the awe is under 0% and if yes correct it
+
+	if peasant_id.worship > 100 then peasant_id.worship = 100	end	-- Check if the worship is over 100% and if yes correct it
+	if peasant_id.worship < 0 then peasant_id.worship = 0	end	-- Check if the worship is under 0% and if yes correct it
+
 	if peasant_id.love > 100 then peasant_id.love = 100	end	-- Check if the love is over 100% and if yes correct it
 	if peasant_id.love < 0 then peasant_id.love = 0	end	-- Check if the love is under 0% and if yes correct it
+
+	if peasant_id.fear > 100 then peasant_id.fear = 100	end	-- Check if the fear is over 100% and if yes correct it
+	if peasant_id.fear < 0 then peasant_id.fear = 0	end	-- Check if the fear is under 0% and if yes correct it
+
+	if peasant_id.hatred > 100 then peasant_id.hatred = 100	end	-- Check if the hatred is over 100% and if yes correct it
+	if peasant_id.hatred < 0 then peasant_id.hatred = 0	end	-- Check if the hatred is under 0% and if yes correct it
 	return peasant_id
 end
 
@@ -34,8 +46,18 @@ function kill(peasant_id)
 	return peasant_id
 end
 
-function change_love(peasant_id, lovevalue)
-	peasant_id.love = peasant_id.love + lovevalue	-- change love by thevalue of the peasant given to the function
+function change_value(peasant_id, value, count)
+	if (value == "awe") then
+		peasant_id.awe = peasant_id.awe + count
+	elseif (value == "worship") then
+		peasant_id.worship = peasant_id.worship + count
+	elseif (value == "love") then
+		peasant_id.love = peasant_id.love + count
+	elseif (value == "fear") then
+		peasant_id.fear = peasant_id.fear + count
+	elseif (value == "hatred") then
+		peasant_id.hatred = peasant_id.hatred + count
+	end
 	checkvalues(peasant_id)
 	return peasant_id
 end
@@ -57,7 +79,7 @@ if testguy then print("He is still alive! However this should be possible...") e
 
 --[[
 	Notes:
-	There are no such things like classes in lua. We got that peasant_id "table" (array) for the peasant and the values (love, worship...) for the stats of the guy
+	There are no such things like classes in lua. We got that peasant_id "table" (array) for the peasant and the values (love, fear...) for the stats of the guy
 	
 	Known errors:
 
